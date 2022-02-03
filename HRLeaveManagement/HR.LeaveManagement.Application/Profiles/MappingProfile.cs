3dab0c52910f7d4242f59f17fb766ebdce3912f8
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2022-02-03 09:25:11 +0000

    End of section 2.15

--- HRLeaveManagement/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HRLeaveManagement/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -9,17 +9,23 @@ namespace HR.LeaveManagement.Application.DTOs.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveRequest
             CreateMap<Domain.LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<Domain.LeaveRequest, LeaveRequestListDto>().ReverseMap();
             CreateMap<Domain.LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
             CreateMap<Domain.LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion
 
+            #region LeaveAllocation
             CreateMap<Domain.LeaveAllocation, LeaveAllocationDto>().ReverseMap();
             CreateMap<Domain.LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
             CreateMap<Domain.LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
 
+            #region LeaveType
             CreateMap<Domain.LeaveType, LeaveTypeDto>().ReverseMap();
-
-            
+            CreateMap<Domain.LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            #endregion
         }
     }
 }
